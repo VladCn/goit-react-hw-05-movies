0fd047084af017c1ca0b86ba8b06c91eefa31f7e
VladZ <vladislav.zaytcev84@gmail.com>
2022-04-19 19:48:42 +0300

    add redirect

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
